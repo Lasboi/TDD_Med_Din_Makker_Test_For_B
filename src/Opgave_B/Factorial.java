bfcd16cc50dcde18fb1c6b8ef62ef08457856784
Emil Nymann <emilnymann96@gmail.com>
2019-09-25 12:37:41 +0200

    Implement findFactorialTwoNumbers() Implement countNumberUp

--- src/Opgave_B/Factorial.java
+++ src/Opgave_B/Factorial.java
@@ -7,17 +7,30 @@ public class Factorial {
     }
 
     public static int findFactorialTwoNumbers(int x, int y) {
-        return 0;
+
+        int result;
+
+        result = x * y;
+
+        return result;
+
     }
 
     public static String outputText( int x, int y, int z) {
         int sum = x * y * z;
 
-
         return "XD"; }
 
-    public static int countNumberUp(int x) { return 0; }
+    public static int countNumberUp(int x) {
 
-    public static int ifAnswerIsZero(int x) { return 0; }
+        int result;
+
+        result = x + 10;
+
+        return result;
+
+    }
+
+    public static int ifAnswerIsZero(int x, int y, int z) { return 0; }
 
 }
