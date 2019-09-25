f07601793b94f67989da04cd4a1ecb74f32cf194
LasBoi <Azlj1689gk>
2019-09-25 10:59:53 +0200

    Lavet metode til Fibonacci

--- src/Opgave_A/Fibonacci.java
+++ src/Opgave_A/Fibonacci.java
@@ -2,6 +2,13 @@ package Opgave_A;
 
 public class Fibonacci {
     public static int getFibonacci(int x) {
-        return 0;
+        int resultat = 0;
+
+        for (int i = 0; i < 3; i++) {
+            resultat = resultat + x;
+        }
+        x = resultat;
+        return x;
+
     }
 }
