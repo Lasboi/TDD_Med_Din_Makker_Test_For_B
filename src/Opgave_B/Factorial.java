ca974d652990a1c6fa5790d16599232500b20806
LasBoi <Azlj1689gk>
2019-09-25 10:20:08 +0200

    Rettet test metoder så de kan testet

--- src/Opgave_B/Factorial.java
+++ src/Opgave_B/Factorial.java
@@ -11,7 +11,7 @@ public class Factorial {
     }
 
     public static String outputText( int x, int y, int z, int sum) {
-        return "Resultatet af " + x + " * " + y + " * " + z + " er " + sum;
+        return "XD";
     }
 
     public static int countNumberUp(int x, int y, int z) { return 0; }
