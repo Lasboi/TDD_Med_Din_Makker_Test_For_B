a3cbc93e9c85425fa65803b0cbc00333e9640bd3
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-09-25 22:22:34 +0200

    fik lavet metoden til ifXisZero og gemmenførte testen

--- src/Opgave_B/Factorial.java
+++ src/Opgave_B/Factorial.java
@@ -2,20 +2,55 @@ package Opgave_B;
 
 public class Factorial {
 
-    public static int findFactorial(int x) {
-        return 0;
+        public static int findFactorial(int x) {
+
+            int result = 1;
+
+            for (int i = x; i > 0; i--) {
+                result *= i;
+            }
+
+            return result;
+
+        }
+
+        public static int findFactorialTwoNumbers(int x, int y) {
+
+            int result;
+
+            result = x * y;
+
+            return result;
+
+        }
+
+        public static String outputText( int x, int y, int z) {
+
+            String result = "";
+
+            int sum = x * y * z;
+            result = "Resultatet af " + x + " * " + y + " * " + z + " er " + sum;
+
+            return result;
+
+        }
+
+        public static int countNumberUp(int x) {
+
+            int result;
+
+            result = x + 10;
+
+            return result;
+
+        }
+
+        public static int ifXIsZero(int x) {
+            int resultat = x;
+                while (x == 0) {
+                    return x + 1;
+                }
+                return resultat;
+
     }
-
-    public static int findFactorialTwoNumbers(int x, int y) {
-        return 0;
-    }
-
-    public static String outputText( int x, int y, int z, int sum) {
-        return "XD";
-    }
-
-    public static int countNumberUp(int x, int y, int z) { return 0; }
-
-    public static int ifAnswerIsZero(int x, int y, int z) { return 0; }
-
 }
--- test/Opgave_B/FactorialTest.java
+++ test/Opgave_B/FactorialTest.java
@@ -21,12 +21,9 @@ public class FactorialTest extends TestCase {
     //Input: x = 0, y = 2, z = 4
     //Forventet output: x = 10, y = 12, z = 14
     public void testTenNumbers() {
-        int x;
-        int y;
-        int z;
-        Factorial.countNumberUp(x = 0, y = 2, z = 4);
-
-        Factorial.countNumberUp(x, y, z);
+        int x = Factorial.countNumberUp(0);
+        int y = Factorial.countNumberUp(2);
+        int z = Factorial.countNumberUp(4);
 
         assertEquals(10, x);
         assertEquals(12, y);
@@ -37,26 +34,23 @@ public class FactorialTest extends TestCase {
     //Input: x = 2, y = 2, z = 4
     //Forventet output: "Resultatet af 2 * 2 * 4 er 16"
     public void testString() {
-        int x;
-        int y;
-        int z;
-        int sum;
+        int x = 2;
+        int y = 2;
+        int z = 4;
         String answer;
 
-       answer = Factorial.outputText(x = 2, y = 2, z = 4 , sum = x * y * z);
-
-       assertEquals("Resultatet af 2 * 2 * 4 er 16", answer);
+        answer = Factorial.outputText(2, 2, 4);
+        assertEquals("Resultatet af 2 * 2 * 4 er 16", answer);
     }
 
     //Edge Test Case - Hvis et tal er nul sæt det til 1
     //Input: x = 0, y = 0, z = 4
     //Forventet output: x = 1, y = 1, z = 4
     public void testIfNumberZero() {
-        int x = 0;
-        int y = 0;
-        int z = 4;
+        int x = Factorial.ifXIsZero(0);
+        int y = Factorial.ifXIsZero(0);
+        int z = Factorial.ifXIsZero(4);
 
-        Factorial.ifAnswerIsZero(x, y, z);
 
         assertEquals(1, x);
         assertEquals(1, y);
@@ -64,16 +58,16 @@ public class FactorialTest extends TestCase {
     }
 
     //Test Case - Få Factorial til at virke på x
-    //Input: x = 12, x = 10
-    //Forventet output: result = 144, result = 100
+    //Input: x = 3, x = 4
+    //Forventet output: result = 6, result = 24
     public void testFactorial() {
-        int x = 12;
+        int x = 3;
         int result = Factorial.findFactorial(x);
-        assertEquals(144, result);
+        assertEquals(6, result);
 
-        x = 10;
+        x = 4;
         result = Factorial.findFactorial(x);
-        assertEquals(100, result);
+        assertEquals(24, result);
 
     }
 }
