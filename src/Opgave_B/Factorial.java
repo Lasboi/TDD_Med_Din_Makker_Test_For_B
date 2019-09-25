18d2019601779b4ba1dbf143282c95ee113bdad2
LasBoi <Azlj1689gk>
2019-09-25 09:30:25 +0200

    Rettet opgaven til så den giver mening for min makker

--- src/Opgave_B/Factorial.java
+++ src/Opgave_B/Factorial.java
@@ -2,4 +2,24 @@ package Opgave_B;
 
 public class Factorial {
 
+    public static int findFactorial(int x) {
+        return 0;
+    }
+
+    public static int findFactorialTwoNumbers(int x, int y) {
+        return 0;
+    }
+
+    public static String outputText(String answer) {
+        return "XD";
+    }
+
+    public static void countNumberUp() {
+
+    }
+
+    public static void ifAnswerIsZero() {
+
+    }
+
 }
--- test/Opgave_B/FactorialTest.java
+++ test/Opgave_B/FactorialTest.java
@@ -7,19 +7,17 @@ public class FactorialTest extends TestCase {
     //Test case - Gange to tal sammen
     //Input: x = 2, y = 4
     //Forventet output: sum = 8
-    public void testFactorial() {
+    public void testTimes() {
         int x = 2;
         int y = 4;
         int sum = 0;
 
-        sum = x * y;
+        sum = Factorial.findFactorialTwoNumbers(x, y);
 
         assertEquals(8, sum);
-
-
     }
 
-    //Test case - Lav tæl x, y og z 10 op
+    //Test case - Tæl x, y og z 10 op
     //Input: x = 0, y = 2, z = 4
     //Forventet output: x = 10, y = 12, z = 14
     public void testTenNumbers() {
@@ -27,11 +25,7 @@ public class FactorialTest extends TestCase {
         int y = 2;
         int z = 4;
 
-        for (int i = 0; i < 10 ; i++) {
-            x++;
-            y++;
-            z++;
-        }
+        Factorial.countNumberUp();
 
         assertEquals(10, x);
         assertEquals(12, y);
@@ -50,12 +44,12 @@ public class FactorialTest extends TestCase {
 
         sum = x * y * z;
 
-       answer = "Resultatet af " + x + " * " + y + " * " + z + " er " + sum;
+       answer = Factorial.outputText(answer);
 
        assertEquals("Resultatet af 2 * 2 * 4 er 16", answer);
     }
 
-    //Edge Test Case - Vi vil ikke gange med 0!
+    //Edge Test Case - Hvis et tal er nul sæt det til 1
     //Input: x = 0, y = 0, z = 4
     //Forventet output: x = 1, y = 1, z = 4
     public void testIfNumberZero() {
@@ -63,31 +57,24 @@ public class FactorialTest extends TestCase {
         int y = 0;
         int z = 4;
 
-        if (x == 0 && y != 0 && z != 0) {
-            x++;
-        }
-        else if (x != 0 && y == 0 && z != 0) {
-            y++;
-        }
-        else if (x != 0 && y != 0 && z == 0) {
-            z++;
-        }
-        else if (x == 0 && y == 0 && z != 0) {
-            x++;
-            y++;
-        }
-        else if (x == 0 && y != 0 && z == 0) {
-            x++;
-            z++;
-        }
-        else if (x != 0 && y == 0 && z == 0) {
-            y++;
-            z++;
-        }
+        Factorial.ifAnswerIsZero();
 
         assertEquals(1, x);
         assertEquals(1, y);
         assertEquals(4, z);
     }
 
+    //Test Case - Få Factorial til at virke på x
+    //Input: x = 12, x = 10
+    //Forventet output: result = 144, result = 100
+    public void testFactorial() {
+        int x = 12;
+        int result = Factorial.findFactorial(x);
+        assertEquals(144, result);
+
+        x = 10;
+        result = Factorial.findFactorial(x);
+        assertEquals(100, result);
+
+    }
 }
